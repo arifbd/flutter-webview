66bb48e93a322c6ab2c3bc5de12190d3e38d32f6
Md Arif <arif991846@gmail.com>
2019-09-10 20:06:33 +0600

    webview in flutter using flutter-webview-plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,73 @@
-import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-String baseUrl = "https://en.wikipedia.org/wiki/Main_Page";
+String baseUrl = 'https://flutter.io';
 
-void main() => runApp(MaterialApp(home: MyWebViewApp()));
+Future main() async => runApp(MyApp());
 
-class MyWebViewApp extends StatefulWidget {
-  @override
-  _MyWebViewAppState createState() => _MyWebViewAppState();
-}
-
-class _MyWebViewAppState extends State<MyWebViewApp> {
-  Completer<WebViewController> _controller = Completer<WebViewController>();
+class MyApp extends StatelessWidget {
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Web View'),
-        actions: <Widget>[
-          NavigationControls(_controller.future),
-        ],
-      ),
-      body: DoubleBackToCloseApp(
-        child: WebView(
-          javaScriptMode: JavaScriptMode.unrestricted,
-          initialUrl: baseUrl,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-        ),
-        snackBar: const SnackBar(content: Text("Tap back again to leave")),
-      ),
-    );
-  }
-}
-
-class NavigationControls extends StatelessWidget {
-  const NavigationControls(this._webViewControllerFuture)
-      : assert(_webViewControllerFuture != null);
-
-  final Future<WebViewController> _webViewControllerFuture;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: _webViewControllerFuture,
-      builder:
-          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
-        final bool webViewReady =
-            snapshot.connectionState == ConnectionState.done;
-        final WebViewController controller = snapshot.data;
-        return Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: !webViewReady
-                  ? null
-                  : () => navigate(context, controller, goBack: true),
+    return MaterialApp(
+      title: 'Flutter WebView Demo',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      routes: {
+        '/': (_) {
+          return WebviewScaffold(
+            url: baseUrl,
+            appBar: AppBar(
+              title: const Text('My WebView'),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.power_settings_new),
+                  onPressed: () {
+                    flutterWebViewPlugin.goBack()?? SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                  },
+                ),
+              ],
             ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: !webViewReady
-                  ? null
-                  : () => navigate(context, controller, goBack: false),
+            withZoom: true,
+            withLocalStorage: true,
+            hidden: true,
+            initialChild: Container(
+              color: Colors.blueAccent,
+              child: const Center(
+                child: Text('Loading.....'),
+              ),
             ),
-          ],
-        );
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goBack();
+                    },
+                  ),
+                  new Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.autorenew),
+                    onPressed: () {
+                      flutterWebViewPlugin.reload();
+                    },
+                  ),
+                  new Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goForward();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       },
     );
-  }
-
-  navigate(BuildContext context, WebViewController controller,
-      {bool goBack: false}) async {
-    bool canNavigate =
-        goBack ? await controller.canGoBack() : await controller.canGoForward();
-    if (canNavigate) {
-      goBack ? controller.goBack() : controller.goForward();
-    } else {
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-            content:
-                Text("${goBack ? 'back' : 'forward'} history unavailable")),
-      );
-    }
   }
 }
